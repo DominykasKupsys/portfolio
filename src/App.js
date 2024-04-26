96f4be52a507d1813dab9b28854bf0dadc7f0afb
DominykasKupsys <dominykas.kupsys@stud.ktmc.lt>
2024-04-26 14:53:46 +0300

    added navigation help

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./css/App.css";
 import WindowsFooter from "./components/WindowsFooter";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Rnd } from "react-rnd";
 import AppContainer from "./components/AppContainer";
 import notepadLogo from "./assets/IMG/notepadappicom.png";
 import folderLogo from "./assets/IMG/openfolder.png";
 import githubLogo from "./assets/IMG/githublogo.webp";
 import InternetExplorerLogo from "./assets/IMG/internetExplorer_icon.png";
+import clippy from "./assets/IMG/clippy.gif";
 
 function App() {
+  const [speechText, setSpeechText] = useState(
+    "Hey, Welcome to Dominykas Portfolio!"
+  );
+  const [showText, setShowText] = useState(true);
+
+  useEffect(() => {
+    const texts = [
+      "Hey, Welcome to Dominykas Portfolio!",
+      "You can learn about my technical and soft skills by double clicking on the corresponding icons!",
+      "You can find out about me and my projects by double clicking on the internet explorer icon and clicking on adress bar!",
+    ];
+
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * texts.length);
+      setSpeechText(texts[randomIndex]);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
   const [AppContainers, setAppContainers] = useState([]);
 
   const handleAppOpen = (appName) => {
@@ -20,6 +40,13 @@ function App() {
 
   const handleAppClose = (appName) => {
     setAppContainers(AppContainers.filter((app) => app !== appName));
+  };
+
+  const toggleTextInvisibility = () => {
+    setShowText(false);
+  };
+  const toggleTextVisibility = () => {
+    setShowText(true);
   };
 
   return (
@@ -67,6 +94,28 @@ function App() {
               onDoubleClick={() => handleAppOpen("Internet Explorer is open")}
             />
             <p>My Projects</p>
+          </div>
+        </Rnd>
+        <Rnd default={{ x: 1100, y: 300 }}>
+          <div className="gif-container">
+            <div
+              className="speech-bubble d-flex flex-column"
+              style={{ visibility: showText ? "visible" : "hidden" }}
+            >
+              <button
+                className="speech-bubble-close-button d-flex ms-auto align-items-center"
+                onClick={toggleTextInvisibility}
+              >
+                X
+              </button>
+              {speechText}
+            </div>
+            <img
+              src={clippy}
+              className="gif"
+              alt="clippy gif"
+              onClick={toggleTextVisibility}
+            />
           </div>
         </Rnd>
         {AppContainers.map((showApp, index) => (
